d534d6b0267b2ff6ce9bcfb2cb37231efd8b8807
Renzo-Cagnola <rencagnola99@gmail.com>
2021-02-01 19:17:24 -0300

    entrega

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor corriendo en el puerto 3000");
+});
+
+const staticFolder = path.resolve(__dirname, "./public");
+app.use(express.static(staticFolder))
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+ 
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+
+app.get("/login" , (req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+}) 
